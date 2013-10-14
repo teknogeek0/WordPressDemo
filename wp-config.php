611d899124c4ada72cf74c00bee8c7ad6d991d55
Chris Munns <chrismunns@gmail.com>
2013-10-13 20:07:36 -0400

    changing the wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@ define('DB_NAME', 'ebdb');
 define('DB_USER', 'ebroot');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin011');
+define('DB_PASSWORD', 'NEWPass');
 
 /** MySQL hostname */
-define('DB_HOST', 'aa1xb52uz05mnht.cfcd3wpyrvoa.us-west-1.rds.amazonaws.com');
+define('DB_HOST', 'NAME.region.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
